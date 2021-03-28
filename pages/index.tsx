ab1baaf117584815a43eb17c76728415615d74cb
tur1ng <mohammadali.ap.2000@gmail.com>
2021-03-28 13:41:18 +0430

    orbit control

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import { Canvas } from "react-three-fiber";
+import { OrbitControls } from "@react-three/drei";
 
 export default function Home() {
   return (
     <Canvas>
+      <OrbitControls />
       <ambientLight intensity={0.5} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
       <pointLight position={[-10, -10, -10]} />
